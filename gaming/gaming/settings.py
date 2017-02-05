0a1d3b1a2020831442878620c5d48075a1d3d435
moidshaikh <moid4facebook@gmail.com>
2017-02-05 17:08:56 +0530

    removed secret ey

--- gaming/gaming/settings.py
+++ gaming/gaming/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hkjbh2^^!9c2z-aa&z0t$t(8pl!pe2tubum=6fy$yv0g!c+1+@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
